e0ed0cefba36934c269702719134b2b58ffb709f
Henrique ☀️ <henriquematheusjob@gmail.com>
2026-07-23 12:41:03 -0300

    Botão
    Coloquei o botão para acionar quando o botão é solto

--- src/main.py
+++ src/main.py
@@ -33,16 +33,15 @@ while True:
         tempo_bloqueio_inicio = time.ticks_ms()
         micro_parada_alertada = False
 
-    elif  peca_passando and not micro_parada_alertada:
-            if time.ticks_diff(tempo_atual, tempo_bloqueio_inicio) > TEMPO_MICRO_PARADA_MS:
-                print("Alerta: Micro-parada detectada!")
-                micro_parada_alertada = True
+    elif peca_passando and not micro_parada_alertada:
+        if time.ticks_diff(tempo_atual, tempo_bloqueio_inicio) > TEMPO_MICRO_PARADA_MS:
+            print("Alerta: Micro-parada detectada!")
+            micro_parada_alertada = True
 
     if peca_passando and leitura_sensorzin > LIMIAR_LIVRE:
         peca_passando = False
         contador_pecas += 1
         print(f"Peca detectada! Total: {contador_pecas}")
-   
 
     leitura_butanzin = butanzin.value()
 
@@ -51,11 +50,11 @@ while True:
 
     if time.ticks_diff(tempo_atual, ultimo_tempo_debounce) > ATRASO_DEBOUNCE_MS:
         if leitura_butanzin == 0 and not butanzinPressionadoAviso:
-            contador_pecas = 0
-            print("Turno resetado com sucesso. Contadores zerados.")
             butanzinPressionadoAviso = True
             
-        elif leitura_butanzin == 1:
+        elif leitura_butanzin == 1 and butanzinPressionadoAviso:
+            contador_pecas = 0
+            print("Turno resetado com sucesso. Contadores zerados.")
             butanzinPressionadoAviso = False
 
     estadoAnteriorButanzin = leitura_butanzin
