c25a211a4bf233ce9100ffdd779841ecc0d1a9e9
Henrique ☀️ <henriquematheusjob@gmail.com>
2026-07-23 14:13:03 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ micro_parada_alertada = False
 LIMIAR_BLOQUEADO = 15000
 LIMIAR_LIVRE = 40000
 
-TEMPO_MICRO_PARADA_MS = 4000  
+TEMPO_MICRO_PARADA_MS = 5200
 
 estadoAnteriorButanzin = 1
 butanzinPressionadoAviso = False
